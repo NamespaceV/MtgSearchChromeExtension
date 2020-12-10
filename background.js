b4a73f228882136551c83a606cca81d1ff9cde9c
zephyr <whatever@apoco.com>
2020-12-10 15:18:39 +0100

    start

---
+++ background.js
@@ -0,0 +1,92 @@
+chrome.contextMenus.create({
+    "title": "Do I have?",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab){
+        var url = `https://deckbox.org/mtg/${info.selectionText}`;
+        chrome.tabs.create({ url: url });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "Scryfall",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab){
+        var url = `https://scryfall.com/search?q=${info.selectionText}`;
+        chrome.tabs.create({ url: url });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "Goldfish",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab)
+    {
+        var url = `https://www.mtggoldfish.com/q?query_string=${info.selectionText}`;
+        var ajax = $.get({
+            url:url,
+            success : function onLoaded(data){
+                var originalUrl = $(data).find("table a")[0].href;
+                var replaceUrl = originalUrl.replace(/^.*\/\/[^\/]*\//, "https://www.mtggoldfish.com/" );
+                chrome.tabs.create({ url: replaceUrl });
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "CardMarkert",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab)
+    {
+        var url = `https://www.cardmarket.com/en/Magic/Products/Search?searchString=${info.selectionText}`;
+        var ajax = $.get({
+            url:url,
+            success : function onLoaded(data){
+                if (!$(data).text().includes("Search Results"))
+                {
+                    chrome.tabs.create({ url: url });
+                    return;
+                }
+                var originalUrl = $(data).find(".table-body .col a")[0].href;
+                var replaceUrl = originalUrl.replace(/^.*\/\/[^\/]*\//, "https://www.cardmarket.com/" );
+                chrome.tabs.create({ url: replaceUrl });
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "Goldfish -- Search",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab){
+        var url = `https://www.mtggoldfish.com/q?query_string=${info.selectionText}`;
+        chrome.tabs.create({ url: url });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "CardMarkert -- Search",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab)
+    {
+        var url = `https://www.cardmarket.com/en/Magic/Products/Search?searchString=${info.selectionText}`;
+        chrome.tabs.create({ url: url });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "Goldfish -- Deck",
+    "contexts":["selection"],
+    "onclick": function genericOnClick(info, tab)
+    {
+        chrome.tabs.executeScript(
+            null,
+            { code: "window.getSelection().toString();" },
+            function parseDeckFromSelectrion(text) {
+                if (!text[0]) return;
+                var url = `https://www.mtggoldfish.com/tools/deck_pricer?deck=${encodeURI(text[0])}`;
+                chrome.tabs.create({ url: url });
+            }
+        );
+    }
+});
